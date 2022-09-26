3945782607d6643dba3802b73885ec03a84fdddf
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-26 12:08:44 +0300

    Initial Commit

---
+++ SwiftUICartPage/SwiftUICartPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICartPageApp.swift
+//  SwiftUICartPage
+//
+//  Created by Amby on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICartPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
